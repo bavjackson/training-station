573ecb0a03d50cb75a3ec29589569dc32eb619f2
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2022-06-05 14:08:26 +0100

    added templates for password reset flow

--- tstation/settings.py
+++ tstation/settings.py
@@ -151,3 +151,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
